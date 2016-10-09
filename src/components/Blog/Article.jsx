a6df5ff7191924dfc048e9f139a83a1ec8b660c6
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-10-09 22:02:58 +0800

    [#1] - Remove useless code.

--- src/components/Blog/Article.jsx
+++ src/components/Blog/Article.jsx
@@ -6,31 +6,26 @@ import BookInfo from '../../components/Blog/BookInfo'
 import ContentParser from '../../components/Blog/ContentParser'
 import './Article.less'
 
-const Article = ({ meta, content, editUrl, filename }) => {
-  
-  
-  return (
-    <article className="col-md-12 aside3-article">
-      <div className="article-header">
-        <h1 id="#identifier">{meta.title}</h1>
-        <div className="article-meta">
-          <span className="words">{content.length} words</span>
-          <a className="content-edit" href={editUrl} target="_blank">{filename}</a>
-        </div>
-        {meta.music ?
-          <MusicBox musicUrl={`http://music.163.com/outchain/player?type=2&id=${meta.music}&auto=0&height=66`} /> : ''}
+const Article = ({ meta, content, editUrl, filename }) => (
+  <article className="col-md-12 aside3-article">
+    <div className="article-header">
+      <h1 id="#identifier">{meta.title}</h1>
+      <div className="article-meta">
+        <span className="words">{content.length} words</span>
+        <a className="content-edit" href={editUrl} target="_blank">{filename}</a>
       </div>
-      <div className="article-content">
-        {meta.layout === 'book' ? meta.books.map((book, index) => <BookInfo key={index} book={book} />) :
-          <ContentParser layout={meta.layout} content={content} />
-        }
-      </div>
-      <div className="article-tags">
-        {meta.tags.map((tag, index) => <Link key={index} to={`/pages/tags/${tag}`}>{tag}</Link>)}
-      </div>
-    </article>
-  )
-}
+      {meta.music && <MusicBox musicUrl={`http://music.163.com/outchain/player?type=2&id=${meta.music}&auto=0&height=66`} />}
+    </div>
+    <div className="article-content">
+      {meta.layout === 'book' ? meta.books.map((book, index) => <BookInfo key={index} book={book} />) :
+        <ContentParser layout={meta.layout} content={content} />
+      }
+    </div>
+    <div className="article-tags">
+      {meta.tags.map((tag, index) => <Link key={index} to={`/pages/tags/${tag}`}>{tag}</Link>)}
+    </div>
+  </article>
+)
 
 Article.propTypes = {
   meta: PropTypes.object.isRequired,

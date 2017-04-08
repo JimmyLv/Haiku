7636bb53b5d54d07f6bfd340b03e48beb594612f
Jimmy Lv <jimmy.jinglv@gmail.com>
2017-04-08 10:19:21 +1000

    [#0] - Upgrade to react 15.5 and refactored PropTypes

--- src/config/Root.dev.jsx
+++ src/config/Root.dev.jsx
@@ -1,5 +1,6 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 import { Provider } from 'react-redux'
+import PropTypes from 'prop-types'
 
 import DevTools from './DevTools'
 import renderRoutes from '../routes'
